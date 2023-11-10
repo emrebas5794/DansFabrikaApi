42e107b6691e2be85cb3e8269899384eee035984
Saye <mohammadjavadnoctis2@gmail.com>
2023-11-11 01:42:19 +0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -24,26 +24,50 @@ const whitelist = [
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      origin: function (origin, callback) {
-        if (!origin) {
-          callback(null, true);
-          return;
-        }
-        if (
-          whitelist.includes(origin) || // Checks your whitelist
-          !!origin.match(/dansfabrika\.com$/) // Overall check for your domain
-        ) {
-          console.log('allowed cors for:', origin);
-          callback(null, true);
-        } else {
-          console.log('blocked cors for:', origin);
-          callback(new ImATeapotException('Not allowed by CORS'), false);
-        }
-      },
-    }
+    // cors: {
+    //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    //   origin: function (origin, callback) {
+    //     if (!origin) {
+    //       callback(null, true);
+    //       return;
+    //     }
+    //     if (
+    //       whitelist.includes(origin) || // Checks your whitelist
+    //       !!origin.match(/dansfabrika\.com$/) // Overall check for your domain
+    //     ) {
+    //       console.log('allowed cors for:', origin);
+    //       callback(null, true);
+    //     } else {
+    //       console.log('blocked cors for:', origin);
+    //       callback(new ImATeapotException('Not allowed by CORS'), false);
+    //     }
+    //   },
+    // }
   });
+
+
+  app.enableCors({
+    origin: [
+      'http://localhost',
+      'https://localhost',
+      'http://localhost:8100',
+      'https://localhost:8100',
+      'http://dansfabrika.com',
+      'https://dansfabrika.com',
+      'http://admin.dansfabrika.com',
+      'https://admin.dansfabrika.com',
+      'http://payment.dansfabrika.com',
+      'https://payment.dansfabrika.com',
+      'http://yeniadmin.dansfabrika.com',
+      'https://yeniadmin.dansfabrika.com',
+      'http://*.dansfabrika.com',
+      'https://*.dansfabrika.com',
+      'capacitor://localhost',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   app.enableVersioning({
